684395e490b54d27403782c32fc84d0987db4e7f
franquero <fran.quero@badgermapping.com>
2022-03-04 14:50:14 +0100

    add first tests

---
+++ gestures.py
@@ -0,0 +1,8 @@
+class Gestures(object):
+    LIST_GESTURES = ['paper', 'rock', 'scissors']
+
+    def __init__(self):
+        pass
+
+    def get_list_of_all_gestures(self):
+        return self.LIST_GESTURES
---
+++ tests.py
@@ -0,0 +1,19 @@
+import unittest
+from game import Game
+from gestures import Gestures
+
+
+class TestRockPaperScissors(unittest.TestCase):
+    def test_when_game_starts_then_there_are_two_players(self):
+        game = Game()
+        self.assertEqual(game.number_of_players, 2)
+
+    def test_when_possible_gestures_are_checked_then_only_rock_paper_and_scissor_values_are_retrieved(self):
+        gestures_values_entity = Gestures()
+        all_possible_gestures = gestures_values_entity.get_list_of_all_gestures()
+        expected_gesture_values = ['paper', 'rock', 'scissors']
+        self.assertCountEqual(all_possible_gestures, expected_gesture_values)
+
+
+if __name__ == '__main__':
+    unittest.main()
